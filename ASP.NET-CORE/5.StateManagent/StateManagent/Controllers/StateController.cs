fe63c8ab87ea58c9dcf2248bf229d7768f54c715
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-01-29 10:06:58 +0300

    State Management Demo

---
+++ ASP.NET-CORE/5.StateManagent/StateManagent/Controllers/StateController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagent.Controllers
+{
+    public class StateController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Encode"] = 101;
+            ViewData["Ename"] = "Moryso";
+
+            ViewBag.Message = "This is comming from ViewBag.Message";
+
+            if(TempData["Country"] == null)
+            {
+                TempData.Add("Country", "Kenya");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection ifc)
+        {
+            return View();
+        }
+    }
+}
