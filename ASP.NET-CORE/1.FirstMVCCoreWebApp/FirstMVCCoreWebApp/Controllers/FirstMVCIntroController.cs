d0b05e35a4eff911880b4c9086c6d82ce716e315
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-01-29 09:39:38 +0300

    First App Demo

---
+++ ASP.NET-CORE/1.FirstMVCCoreWebApp/FirstMVCCoreWebApp/Controllers/FirstMVCIntroController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCCoreWebApp.Controllers
+{
+    public class FirstMVCIntroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Index2()
+        {
+            return ("Hello from Index2");
+        }
+
+        public ViewResult Index3()
+        {
+            return View("Index33");
+        }
+
+        public ViewResult Index4()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View("index4", viewModel);
+        }
+    }
+}
