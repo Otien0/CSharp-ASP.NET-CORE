fb072b92d49945dec83b4d09ae62abe139ad5b1d
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-01-29 09:32:53 +0300

    Web App Demo

---
+++ ASP.NET-CORE/0.WebApplication1/WebApplication1/MiddleWares/RequestQueryStringMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.MiddleWares
+{
+    public class RequestQueryStringMiddleware
+    {
+        private RequestDelegate next;
+
+        public RequestQueryStringMiddleware(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.RequestMethod = HttpMethods.Get 
+                && context.Request.Query["iscertified"] == "true")
+            {
+                await context.Response.WriteAsync("Message from Class-Based Middleware \n");
+            }
+            await next(context);
+        }
+    }
+}
