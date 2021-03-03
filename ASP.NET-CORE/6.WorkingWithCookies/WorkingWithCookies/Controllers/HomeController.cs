76a63a441330c445dc2fe9e0dabdef911acf244c
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-03-03 11:51:23 +0300

    Update Files

---
+++ ASP.NET-CORE/6.WorkingWithCookies/WorkingWithCookies/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithCookies.Models;
+
+namespace WorkingWithCookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            //_httpContextAccessor.HttpContext.Response.Cookies.Delete("UserName");
+
+            //CookieOptions option = new CookieOptions();
+            //option.Expires = DateTime.Now.AddMinutes(5);
+            //_httpContextAccessor.HttpContext.Response.Cookies.Append("UserName", "NinjA");
+
+            //_httpContextAccessor.HttpContext.Session.SetString("MyUserName", "NinjA");
+            //ViewBag.SessionID = _httpContextAccessor.HttpContext.Session.Id;
+
+            _httpContextAccessor.HttpContext.Session.Remove("MyUserName");
+            //_httpContextAccessor.HttpContext.Session.Clear();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET-CORE/6.WorkingWithCookies/WorkingWithCookies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkingWithCookies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
