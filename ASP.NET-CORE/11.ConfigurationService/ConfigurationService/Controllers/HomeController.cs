224871050451518253e262240fd8e9e97c204bca
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-03-03 16:42:10 +0300

    Update Files

---
+++ ASP.NET-CORE/11.ConfigurationService/ConfigurationService/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using ConfigurationService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
+
+        private MyApplicationKeysOptions options;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, IOptions<MyApplicationKeysOptions> _options)
+        {
+            _logger = logger;
+            Configuration = configuration;
+            options = _options.Value;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.LogLevel = Configuration["Logging:LogLevel:Default"];
+
+            ViewBag.AzureSQLServerURL = options.AzureSQLServerURL;
+            ViewBag.EmployeePhotoUploadPath = options.EmployeePhotoUploadPath;
+            ViewBag.SMTPServer = options.SMTPServer;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET-CORE/11.ConfigurationService/ConfigurationService/MyApplicationKeysOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationService
+{
+    public class MyApplicationKeysOptions
+    {
+        public string SMTPServer { get; set; }
+        public string AzureSQLServerURL { get; set; }
+        public string EmployeePhotoUploadPath { get; set; }
+    }
+}
