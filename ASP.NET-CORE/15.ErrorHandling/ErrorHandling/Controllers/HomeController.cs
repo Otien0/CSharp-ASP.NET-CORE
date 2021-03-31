7fd260a2a5ce4402899dfb9d0b0d6ad6b1b9ca06
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-03-31 20:51:05 +0300

    ErrorHandling

---
+++ ASP.NET-CORE/15.ErrorHandling/ErrorHandling/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ErrorHandling.Filters;
+using ErrorHandling.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [CustomExceptionFilter]
+        public IActionResult Index()
+        {
+            int a, b, c;
+            a = 10;
+            b = 0;
+            c = a / b;
+
+            ViewBag.C = c.ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            return View(exceptionHandlerPathFeature.Error);
+        }
+    }
+}
---
+++ ASP.NET-CORE/15.ErrorHandling/ErrorHandling/Filter/CustomExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+
+        public override void OnException(ExceptionContext filterContext)
+        {
+            //Do Logging
+
+            ViewResult result = new ViewResult();
+            result.ViewName = "/Views/Shared/ErrorMessage.cshtml";
+
+            result.ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary());
+
+            result.ViewData.Model = filterContext.Exception;
+
+            result.ViewData["Controller"] = (string)filterContext.RouteData.Values["controller"];
+            result.ViewData["Action"] = (string)filterContext.RouteData.Values["action"];
+
+            filterContext.Result = result;
+            filterContext.ExceptionHandled = true;
+
+        }
+
+
+    }
+}
