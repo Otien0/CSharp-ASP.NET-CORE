1895ecaa4f4dc71dc9a13b999dd2d4bdc1000495
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-03-31 20:41:57 +0300

    StoringUserSecrets

---
+++ ASP.NET-CORE/12.StoringUserSecrets/StoringUserSecrets/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using StoringUserSecrets.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoringUserSecrets.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration iconfiguration)
+        {
+            _logger = logger;
+            Configuration = iconfiguration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.APIKey = Configuration["MySecretKeys:APIKey"];
+            ViewBag.DBPwd = Configuration["MySecretKeys:DBPwd"];
+            ViewBag.AdminAccount = Configuration["MySecretKeys:AdminAccount"];
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET-CORE/12.StoringUserSecrets/StoringUserSecrets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoringUserSecrets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
