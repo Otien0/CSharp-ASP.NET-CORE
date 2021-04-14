15da3894a0ffd6e52857016ce49792faf4516364
Mourice Otieno Oduor <otienomaurice364@gmail.com>
2021-04-14 18:01:02 +0300

    Builtin TagHelpers

---
+++ ASP.NET-CORE/17.CustomTagHelpers/CustomTagHelpers/TagHelpers/TrTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomTagHelpers.TagHelpers
+{
+
+    //[HtmlTargetElement("tr", Attributes = "bg-color,text-color", ParentTag = "thead")]
+    //[HtmlTargetElement("tr", Attributes = "bg-color,text-color", ParentTag = "tbody")]
+    //[HtmlTargetElement("tr", ParentTag = "tbody")]
+
+    [HtmlTargetElement("*", Attributes = "bg-color,text-color")]
+    public class TrTagHelper : TagHelper
+    {
+        public string BgColor { get; set; } = "dark";
+        public string TextColor { get; set; } = "white";
+
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class",
+            $"bg-{BgColor} text-center text-{TextColor}");
+        }
+    }
+}
---
+++ ASP.NET-CORE/18.BuiltInTagHelpers/BuitinTagHelpers/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuiltInTagHelpers.Models;
+
+namespace BuiltInTagHelpersDemos.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductsData pData;
+
+      
+
+        public ProductController(ProductsData cData)
+        {
+            pData = cData;
+        }
+
+        public IActionResult Index()
+        {
+            return View(pData.Products);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            return View(pData.Products.FirstOrDefault(a => a.ProductID == id));
+        }
+
+        [Route("/Product/ShowAllProducts", Name = "ListOfProducts")]
+        public IActionResult ShowAllProducts()
+        {
+            return View(pData.Products);
+        }
+
+    }
+}
