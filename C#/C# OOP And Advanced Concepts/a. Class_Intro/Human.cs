b7ddce74fead08ab47817935df8481b6bf4845ee
mourice-oduor <otienomaurice364@gmail.com>
2022-01-31 09:34:57 +0300

    C# OOP-Classes

---
+++ C#/C# OOP And Advanced Concepts/a. Class_Intro/Human.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Intro
+{
+    class Human
+    {
+        // why private? https://softwareengineering.stackexchange.com/questions/143736/why-do-we-need-private-variables
+        private int age;
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private bool isHuman = true;
+
+        public Human(int theAge, string firstName, string lastName, string eyeColor)
+        {
+            age = theAge;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        }
+
+
+        public void IntroduceOnceself()
+        {
+            Console.WriteLine("My name is {0} {1}, and my eye color is {2}", firstName, lastName, eyeColor);
+
+        }
+
+
+    }
+}
---
+++ C#/C# OOP And Advanced Concepts/a. Class_Intro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Human morris = new Human(28, "Morris", "Otieno", "blue");
+            Human effie = new Human(32, "Effie", "Otieno", "awesome");
+            morris.IntroduceOnceself();
+            effie.IntroduceOnceself();
+            Console.Read();
+        }
+    }
+}
