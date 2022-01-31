b70e7ffee1792f3b5c27ecdfe3f2e75f257fa11b
mourice-oduor <otienomaurice364@gmail.com>
2022-01-31 09:52:05 +0300

    C# OOP-Properties

---
+++ C#/C# OOP And Advanced Concepts/c. Properties/Properties/Box.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Box
+    {
+        // member variable
+        private string color = "white";
+        private int length;
+        private int height;
+        //private int width;
+        private int volume;
+
+        // auto - implemented property - enter "prop" + press double tab
+        public int Width { get; set; }
+
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            this.Width = width;
+        }
+
+
+        public int Volume
+        {
+            get
+            {
+                return Heigth * Width * Length;
+            }
+
+            set
+            {
+                volume = value;
+            }
+
+        }
+
+
+        // read only
+        public int Heigth
+        {
+            get
+            {
+                return height;
+            }
+            /*
+            set
+            {
+                if (value < 0)
+                    value = -value;
+                    
+                    //throw new Exception("Size should be positive");
+                height = value;
+            }
+            */
+        }
+
+
+        public int Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        /*
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value;
+            }
+        }
+        */
+        /*
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+        
+        public int GetLength()
+        {
+            return this.length;
+        }
+        */
+
+        public int FrontSurface
+        {
+            get { return height * length; }
+
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0} and height is {1} and width is {2} so the volume is {3}",
+                length, height, Width, volume = Width * height * length);
+        }
+    }
+}
---
+++ C#/C# OOP And Advanced Concepts/c. Properties/Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(5, 4, 10);
+            Console.WriteLine("volume is {0}", box.Volume);
+            Console.WriteLine("Front surface of the box is {0}", box.FrontSurface);
+            box.DisplayInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
