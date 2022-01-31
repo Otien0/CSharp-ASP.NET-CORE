273900b280e45efa57bf25ef3c3c39369ad54eb6
mourice-oduor <otienomaurice364@gmail.com>
2022-01-31 09:58:50 +0300

    C# OOP-Member-Methods

---
+++ C#/C# OOP And Advanced Concepts/d. Member Methods/MemberMethods/Members.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberMethods
+{
+    internal class Members
+    {
+        // member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary;
+
+        // member - public field
+        public int age;
+
+        // member - property - exposes jobTitle safely - properties start with a capital letter
+        public string JobTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        }
+
+        // public member Method - can be called from other classes
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi, my name is {0}, and my job title is {1}. I'm {2} years old", memberName, jobTitle, age);
+            }
+
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine("My salary is {0}", salary);
+        }
+
+        // member constructor
+        public Members()
+        {
+            age = 30;
+            memberName = "Effie";
+            salary = 60000;
+            jobTitle = "Developer";
+            Console.WriteLine("Object created");
+        }
+
+        // member - finalizer - destructor
+        ~Members()
+        {
+            // cleanup statements
+            Console.WriteLine("Deconstruction of Members object");
+            Debug.Write("Destruction of Members object");
+        }
+    }
+}
---
+++ C#/C# OOP And Advanced Concepts/d. Member Methods/MemberMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(false);
+            Console.ReadKey();
+        }
+    }
+}
