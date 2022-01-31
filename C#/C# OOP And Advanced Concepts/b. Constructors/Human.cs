ca110b260ec75f4c423b9c33233114b71c9b6c43
mourice-oduor <otienomaurice364@gmail.com>
2022-01-31 09:44:54 +0300

    C# OOP-Constructors

---
+++ C#/C# OOP And Advanced Concepts/b. Constructors/Human.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    // this class is a blueprint for a datatype 
+    internal class Human
+    {
+        // member variable
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        // default constructor
+        public Human()
+        {
+            Console.WriteLine("Constructor called. Object created");
+        }
+
+        public Human(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        }
+
+        public Human(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        // parameterized constructor
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+
+
+        // member method
+        public void IntroduceMyself()
+        {
+            if (age != 0 && lastName != null && eyeColor != null && firstName != null)
+                Console.WriteLine("Hi, I'm {0} {1} and I am {2} years old. My eye color is {3}.", firstName, lastName, age, eyeColor);
+            else if (age != 0 && lastName != null && firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} {1} and I am {2} years old.", firstName, lastName, age);
+            }
+            else if (lastName != null && firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} {1}.", firstName, lastName);
+            }
+            else if (firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} ", firstName);
+            }
+
+        }
+    }
+}
---
+++ C#/C# OOP And Advanced Concepts/b. Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human morris = new Human("Morris", "Otieno", "grey", 28);
+            morris.IntroduceMyself();
+
+            Human effie = new Human("Effie", "Otieno", "blue");
+            effie.IntroduceMyself();
+
+            Human basicHuman = new Human();
+            basicHuman.IntroduceMyself();
+
+            Human natalie = new Human("Natalie");
+            natalie.IntroduceMyself();
+
+            Human michael = new Human("Michael", "Miller");
+            michael.IntroduceMyself();
+
+            Human frank = new Human("Frank", "Walter", 25);
+            frank.IntroduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}
