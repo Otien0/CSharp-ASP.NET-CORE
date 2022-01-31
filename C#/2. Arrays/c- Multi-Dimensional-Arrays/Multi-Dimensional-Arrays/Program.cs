16ce1d086cb035e775c33d0f671817e711bb1e2e
mourice-oduor <otienomaurice364@gmail.com>
2022-01-31 11:08:16 +0300

    Multi Dimensional Arrays

---
+++ C#/2. Arrays/c- Multi-Dimensional-Arrays/Multi-Dimensional-Arrays/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Dimensional_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare and initualize a 2-dimensional Array
+            int[,] array2D = new int[,]
+            {
+                {11, 12, 13 },
+                {21, 22, 23 },
+                {31, 32, 33 }
+            };
+
+            //We access the Value 23
+            //Note, that Indexes start with 0
+            Console.WriteLine(array2D[1, 2]);
+
+
+            //3-Dimensions are more complex, but the Conzept is the same
+            string[,,] array3D = new string[,,]
+            {
+                {
+                    {"000", "001", "002" },
+                    {"010", "011", "012" },
+                    {"020", "021", "022" }
+                },
+                {
+                    {"100", "101", "102" },
+                    {"110", "111", "112" },
+                    {"120", "121", "122" }
+                },
+                {
+                    {"200", "201", "202" },
+                    {"210", "211", "212" },
+                    {"220", "221", "222" }
+                },
+                {
+                    {"Hi", "I", "am" },
+                    {"also", "part", "of" },
+                    {"the", "3-dimensional", "array" }
+                }
+
+            };
+
+            //We access the Value "201"
+            Console.WriteLine(array3D[2, 0, 1]);
+
+            //Hi
+            Console.WriteLine(array3D[3, 0, 0]);
+
+            //Will give us the Dimension of the corresponding Array
+            int amountDimensions = array3D.Rank;
+            Console.WriteLine("The Dimension is {0}", amountDimensions);
+
+            //Multi-dimensional Arrays can also be created that way:
+            string[,] arr2D = { { "00", "01" } };
+
+            Console.WriteLine(arr2D[0, 0]);
+
+            //To keep the Console open
+            Console.Read();
+        }
+    }
+}
