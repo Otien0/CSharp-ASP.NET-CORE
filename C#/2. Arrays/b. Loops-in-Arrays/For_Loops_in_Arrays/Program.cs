61b275eb88f9c8152bda51ea501453e00d9bb48d
mourice-oduor <otienomaurice364@gmail.com>
2022-01-31 11:01:59 +0300

    Loops in Arrays

---
+++ C#/2. Arrays/b. Loops-in-Arrays/For_Loops_in_Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loops_in_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i + 10;
+            }
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine("Element{0} = {1}", j, nums[j]);
+
+                //Console.WriteLine("__________________________");
+
+            }
+            Console.WriteLine("__________________________");
+
+
+            int counter = 0;
+            foreach (int k in nums)
+            {
+
+                Console.WriteLine("Element{0} = {1}", counter, k);
+            }
+
+            Console.ReadKey();
+
+            // Create an array with the names of five friends
+            // greet all of them with a foreach loop
+
+            string[] myFriends = { "Michael", "Wlad", "Ilja", "Andy", "Daniel" };
+
+            foreach (string name in myFriends)
+            {
+                Console.WriteLine("Hi there {0}, my friend", name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
