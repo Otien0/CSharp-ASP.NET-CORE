70a235abb0be8dd50e536db6797124e5ca56fb7c
mourice-oduor <otienomaurice364@gmail.com>
2022-02-03 10:47:31 +0300

    File Input Methods -Polymorphism

---
+++ C#/4-Polymorphism/ReadFile/ReadFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading Tect from a File
+            //exchange the adress of the file with the one you want to use
+            string text = System.IO.File.ReadAllText(@"D:\C# Masterclass Course\Projects\Assets\textfile.txt");
+
+            Console.WriteLine("Textfile contains the following text: {0}", text);
+
+            //Receive the Text line by line.
+            string[] lines = System.IO.File.ReadAllLines(@"D:\C# Masterclass Course\Projects\Assets\textfile.txt");
+
+            Console.WriteLine("Content of the file line by line:");
+            foreach(string line in lines)
+            {
+                //\t is a tab
+                Console.WriteLine{ "\t" + line};
+            }
+
+            //To keep the Console open.
+            Console.Read();
+        }
+    }
+}
